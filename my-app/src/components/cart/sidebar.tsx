475fa350711d2024fe4bf4ac3be9a95647cf32a2
Igor Ferraz <igormanoelferraz2022@gmail.com>
2024-03-13 20:56:19 -0300

    update

--- my-app/src/components/cart/sidebar.tsx
+++ my-app/src/components/cart/sidebar.tsx
@@ -1,14 +1,28 @@
+'use client'
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components//ui/sheet"
 import { Button } from "@/components/ui/button"
 import { MenuIcon } from "lucide-react"
 import { Separator } from "@/components/ui/separator"
+import { useCartStore } from "@/stores/cart-store"
+import { CartItem } from "./item"
 
 export const Sidebar = () => {
+    const { cart } = useCartStore(state => state);
+
+    let subtotal = 0;
+    for(let item of cart) {
+        subtotal += item.quantity * item.product.price
+    }
+
     return(
         <Sheet>
             <SheetTrigger asChild>
-                <Button>
+                <Button className="relative">
                     <MenuIcon/>
+                    {
+                        cart.length > 0 &&
+                            <div className="absolute size-3 bg-red-600 rounded-full -right-1 -top-1"></div>
+                    }
                 </Button>
             </SheetTrigger>
 
@@ -20,16 +34,22 @@ export const Sidebar = () => {
                 </SheetHeader>
 
                 <div className="flex flex-col gap-5 my-3">
-                    ....
+                    {cart.map(item => (
+                        <CartItem key={item.product.id} item={item}/>
+                    ))}
                 </div>
                 <Separator className="my-4"/>
                 <div className="flex justify-between items-center text-xs">
                     <div>Subtotal:</div>
-                    <div>...</div>
+                    <div>R$ {subtotal.toFixed(2)}</div>
                 </div>
                 <Separator className="my-4"/>
                 <div className="text-center">
-                    <Button>Finalizar Compra</Button>
+                    <Button
+                        disabled={cart.length === 0}
+                        >
+                            Finalizar Compra
+                    </Button>
                 </div>
 
             </SheetContent>
