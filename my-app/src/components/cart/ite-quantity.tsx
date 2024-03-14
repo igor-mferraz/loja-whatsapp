a3e9cbddcd3c420a6984ffa5c037319bef2f07a3
Igor Ferraz <igormanoelferraz2022@gmail.com>
2024-03-14 13:14:15 -0300

    update

--- my-app/src/components/cart/ite-quantity.tsx
+++ my-app/src/components/cart/ite-quantity.tsx
@@ -1,13 +1,44 @@
+import { useCartStore } from "@/stores/cart-store"
 import { Cart } from "@/types/cart"
+import { Button } from "@/components/ui/button"
+import { MinusIcon, PlusIcon } from "lucide-react"
 
 type Props = {
     cartItem: Cart
 }
 
 export const CartItemQuantity = ({ cartItem }: Props) => {
+    const { upsertCartItem } = useCartStore(state => state);
+
+    const handleAdd = () => {
+        upsertCartItem(cartItem.product, 1)
+    }
+
+    const handleRemove = () => {
+        upsertCartItem(cartItem.product, -1)
+    }
+
     return(
-        <div>
-            {cartItem.quantity}
+        <div className="flex items-center gap-2">
+            <Button
+                onClick={handleAdd}
+                variant={"outline"}
+                size={"icon"}
+                className="size-5"
+                >
+                <PlusIcon className="size-3" />
+            </Button>
+            <div className="text-xs">
+                {cartItem.quantity}
+            </div>
+            <Button
+                onClick={handleRemove}
+                variant={"outline"}
+                size={"icon"}
+                className="size-5"
+            >
+                <MinusIcon/>
+            </Button>              
         </div>
     )
 }
